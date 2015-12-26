5455aa37d9c517ae4a33f445c4dcc8e33b90eef7
QH_Company <381397155@qq.com>
2015-12-26 10:20:05 +0800

    deleted some file

--- searchpy/spiders/searchpy_spider.py
+++ searchpy/spiders/searchpy_spider.py
@@ -20,16 +20,17 @@ class SearchpySpider(scrapy.Spider):
 	
 	def parse(self,response):
 		filename = response.url.split("/")[-1][7]
-		with open(filename,'wb') as f:
-			f.write(response.body)
-			f.close()
+		#with open(filename,'wb') as f:
+		#	f.write(response.body)
+		#	f.close()
 		#filename = response.url.split("/")[-2]
-		for sel in response.xpath('//dl[re:test(@class,"search-list")]/dd[re:test(@class,"search-link")]'):
+		for sel in response.xpath('//dl[re:test(@class,"search-list")]/dt'):
 			item = SearchpyItem()
 			item['name'] = sel.xpath('a/text()').extract()
 			item['url'] = sel.xpath('a/@href').extract()
 			item['description'] = sel.xpath('text()').extract()
-			with open(filename,'wb') as f:
-				f.write("%s"%sel.xpath('a/@href').extract())
+			result = "%s %s\n"%(sel.xpath('a/text()').extract(),sel.xpath('a/@href').extract())
+			with open(filename,'a') as f:
+				f.write(result)
 			#print sel.xpath('a/@href').extract()
 			#print sel.xpath('//dl[re:test(@class,"search-list")]')
